345d1819f1a5657300b4b8af0653395617497c7a
thiagovcardoso <36796432+thiagoDeviner@users.noreply.github.com>
2021-02-12 23:06:21 -0300

    Finalizando site bikcraft
    Finalizando html5, CSS e Javascript do site bikcraft.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-new SimpleSlide({
+if (window.SimpleSlide) {
+
+	new SimpleSlide({
 		slide: "quote", // nome do atributo data-slide="principal"
 		time: 5000 // tempo de transição dos slides
 	});
@@ -8,3 +10,9 @@ new SimpleSlide({
 		time: 5000,
 		nav: true
 	});
+
+}
+
+if (window.SimpleAnime) {
+	new SimpleAnime();
+}
